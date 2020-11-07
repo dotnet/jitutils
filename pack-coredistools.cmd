9947c59d3dcb1e47fd0ce5666066fce42c1edda8
Egor Chesakov <Egor.Chesakov@microsoft.com>
2020-11-06 18:43:25 -0800

    One CoreDisTools NuGet package (#298)
    * Stage binaries and then pack them up into one package in coredistools.yml
    
    * Build one NuGet package in pack-coredistools.cmd
    
    * One NuGet package for all platforms in Microsoft.NETCore.CoreDisTools.nuspec
    
    * Remove *.nuspec that we don't anymore

--- pack-coredistools.cmd
+++ pack-coredistools.cmd
@@ -5,6 +5,16 @@ set RootDirectory=%~dp0
 set SourcesDirectory=%RootDirectory%src
 set PackagesDirectory=%RootDirectory%artifacts\pkg
 
+set BinariesDirectory=%~f1
+
+if "%BinariesDirectory%"=="" (
+    echo ERROR: Binaries directory is not specified
+    exit /b 1
+) else if not exist "%BinariesDirectory%" (
+    echo ERROR: Binaries directory does not exist: %BinariesDirectory%
+    exit /b 1
+)
+
 where /q nuget.exe
 
 if %ERRORLEVEL% neq 0 (
@@ -12,48 +22,16 @@ if %ERRORLEVEL% neq 0 (
     exit /b 1
 )
 
-if /i "%1"=="portable" (
-    nuget.exe pack ^
-        "%SourcesDirectory%\coredistools\.nuget\Microsoft.NETCore.CoreDisTools.nuspec" ^
-        -OutputDirectory "%PackagesDirectory%" ^
-        -BasePath %RootDirectory% ^
-        -NonInteractive
-
-    if %ERRORLEVEL% neq 0 goto :NuGetNonZeroExitStatus
-    exit /b 0
-)
-
-call :EnsureSupportedTargetOSArchitecture %1
-
-if %ERRORLEVEL% neq 0 (
-    echo ERROR: Unknown target OS and architecture: %TargetOSArchitecture%
-    exit /b 1
-)
-
-set TargetOSArchitecture=%1
-set BinariesDirectory=%~2
-
-if "%BinariesDirectory%"=="" (
-    set BinariesDirectory=%RootDirectory%artifacts\%TargetOSArchitecture%\bin
-)
-
 nuget.exe pack ^
-    "%SourcesDirectory%\coredistools\.nuget\runtime.%TargetOSArchitecture%.Microsoft.NETCore.CoreDisTools.nuspec" ^
+    "%SourcesDirectory%\coredistools\.nuget\Microsoft.NETCore.CoreDisTools.nuspec" ^
     -OutputDirectory "%PackagesDirectory%" ^
     -BasePath %RootDirectory% ^
     -Properties BinariesDirectory="%BinariesDirectory%" ^
     -NonInteractive
 
-if %ERRORLEVEL% neq 0 goto :NuGetNonZeroExitStatus
+if %ERRORLEVEL% neq 0 (
+    echo ERROR: nuget pack exited with code %ERRORLEVEL%
+    exit /b 1
+)
 
 exit /b 0
-
-:NuGetNonZeroExitStatus
-
-echo ERROR: nuget pack exited with code %ERRORLEVEL%
-exit /b 1
-
-:EnsureSupportedTargetOSArchitecture
-
-for %%I in (linux-arm linux-arm64 linux-x64 osx-x64 win-arm win-arm64 win-x64 win-x86) do (if /i "%%I"=="%1" exit /b 0)
-exit /b 1
