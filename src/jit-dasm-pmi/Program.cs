b19db6c8a30fc1717a554f5ef0f7f8dc14778188
Adeel Mujahid <3840695+am11@users.noreply.github.com>
2025-04-15 00:18:34 +0300

    Update command-line-api to beta5 (#420)
    Sync with runtime version
    https://github.com/dotnet/runtime/commit/a58e9239e3dbe2259621507df51f7dcbfd849ae4.

--- src/jit-dasm-pmi/Program.cs
+++ src/jit-dasm-pmi/Program.cs
@@ -72,14 +72,11 @@ namespace ManagedCodeGen
             return errorCount;
         }
 
-        private T Get<T>(CliOption<T> option) => _command.Result.GetValue(option);
-        private T Get<T>(CliArgument<T> arg) => _command.Result.GetValue(arg);
+        private T Get<T>(Option<T> option) => _command.Result.GetValue(option);
+        private T Get<T>(Argument<T> arg) => _command.Result.GetValue(arg);
 
         private static int Main(string[] args) =>
-            new CliConfiguration(new JitDasmPmiRootCommand(args).UseVersion())
-            {
-                EnableParseErrorReporting = true
-            }.Invoke(args);
+            new CommandLineConfiguration(new JitDasmPmiRootCommand(args).UseVersion()).Invoke(args);
 
         public List<AssemblyInfo> GenerateAssemblyWorklist()
         {
